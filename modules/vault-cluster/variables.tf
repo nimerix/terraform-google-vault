bcca206c22d98f6d7e95e1c1f798d28541e8668a
Michael Pearce <michael@nurulize.com>
2018-08-10 18:50:52 -0700

    adding support for VPC network subnets

--- modules/vault-cluster/variables.tf
+++ modules/vault-cluster/variables.tf
@@ -69,6 +69,10 @@ variable "network_name" {
   default = "default"
 }
 
+variable "subnetwork_name" {
+  description = "The name of the VPC Subnetwork where resources should be created."
+  default = ""
+}
 variable "custom_tags" {
   description = "A list of tags that will be added to the Compute Instance Template in addition to the tags automatically added by this module."
   type = "list"
